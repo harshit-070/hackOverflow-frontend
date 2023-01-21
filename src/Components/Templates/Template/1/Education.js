efa4e2b847263425dd20803a07024e451454da7f
Jayant <sahujayant046@gmail.com>
2023-01-21 08:18:37 +0530

    Templates

---
+++ src/Components/Templates/Template/1/Education.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Education = () => {
+    return (
+        <div class="rela-block page">
+            <div class="side-bar">
+                <p class="rela-block caps side-header">Education</p>
+                <p class="rela-block list-thing">Advanced potion making</p>
+                <p class="rela-block list-thing">Degree in popping and locking</p>
+                <p class="rela-block list-thing">Knitting game on point</p>
+                <p class="rela-block list-thing">Culinary af</p>
+            </div>
+        </div>
+    )
+}
+
+export default Education
---
+++ src/Components/Templates/Template/2/Education.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const Education = () => {
+    return (
+        <div class="right">
+            <h3>Educational Qualifications</h3>
+            <table>
+                <tr id="heading">
+                    <td>Qualification</td>
+                    <td>Board</td>
+                    <td>Percentage / Grades</td>
+                    <td>Year</td>
+                </tr>
+                <tr>
+                    <td>S.S.C</td>
+                    <td>G.S.E.B India</td>
+                    <td>75.57%</td>
+                    <td>2004</td>
+                </tr>
+                <tr>
+                    <td>H.S.C (Science Stream)</td>
+                    <td>G.S.E.B India</td>
+                    <td>72.40%</td>
+                    <td>2006</td>
+                </tr>
+                <tr>
+                    <td>GDCA (Grad. Dipl in Computer Applications)</td>
+                    <td>NIE (National Institute Of Engineering), Mumbai, India</td>
+                    <td>A Grade</td>
+                    <td>2008</td>
+                </tr>
+                <tr>
+                    <td>B.Sc. (Hons) - Applied Accounting</td>
+                    <td>Oxford Brookes University</td>
+                    <td>2:1 – Upper Second Class Honours</td>
+                    <td>2012</td>
+                </tr>
+                <tr>
+                    <td>Chartered Accounting - ACCA</td>
+                    <td>ACCA Glasgow, UK</td>
+                    <td>Passed (1st Attempt)</td>
+                    <td>2013</td>
+                </tr>
+            </table>
+        </div>
+    )
+}
+
+export default Education
